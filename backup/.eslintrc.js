99beec99469afae0d8f338a8a8803dd127525374
종원 <56539682+Bruce0203@users.noreply.github.com>
2022-06-25 12:31:40 +0000

    add modules

---
+++ backup/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: "eslint:recommended",
+
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        jest: true
+    },
+
+    globals: {
+        pskl: true,
+        phantom: true
+    },
+
+
+    rules: {
+        "no-console": "off",
+        "comma-dangle": ["error", "never"],
+        "object-curly-spacing": ["error", "always"],
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"]
+    }
+};
