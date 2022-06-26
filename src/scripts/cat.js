0fb0d60302c78adfb86cb9caf1b8aae953d21520
종원 <56539682+Bruce0203@users.noreply.github.com>
2022-06-26 04:27:41 +0000

    commit or green light of test ok

--- src/scripts/cat.js
+++ src/scripts/cat.js
@@ -1,37 +1,17 @@
 document.addEventListener(START, () => {
-    app.loader
-    .add('assets/kat.json')
-    .load(onAssetsLoaded);
+    newAnim("kat", (anim) => {
+        anim.x = app.screen.width * (10/40);
+        anim.y = app.screen.height * (17/40);
+        anim.anchor.set(0.5);
+        anim.animationSpeed = 0.2;
+        anim.play();
+        group = new PIXI.Container();
+        app.stage.addChild(group)
+        group.addChild(anim);
+
+        // Animate the rotation
+        app.ticker.add(() => {
+        });
+    })
 })
 
-function onAssetsLoaded() {
-    // create an array of textures from an image path
-    const frames = [];
-
-    for (let i = 0; i < 4; i++) {
-        const val = i < 4 ? `${i}` : i;
-
-        // magically works since the spritesheet was loaded with the pixi loader
-        frames.push(PIXI.Texture.from(`kat${val}.png`));
-    }
-
-    // create an AnimatedSprite (brings back memories from the days of Flash, right ?)
-    const anim = new PIXI.AnimatedSprite(frames);
-
-    /*
-     * An AnimatedSprite inherits all the properties of a PIXI sprite
-     * so you can change its position, its anchor, mask it, etc
-     */
-    anim.x = app.screen.width * (10/40);
-    anim.y = app.screen.height * (17/40);
-    anim.anchor.set(0.5);
-    anim.animationSpeed = 0.2;
-    anim.play();
-    group = new PIXI.Container();
-    app.stage.addChild(group)
-    group.addChild(anim);
-
-    // Animate the rotation
-    app.ticker.add(() => {
-    });
-}
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -2,3 +2,21 @@ function center(sprite) {
     sprite.pivot.x = sprite.width/2
     sprite.pivot.y = sprite.height/2
 }
+
+function newAnim(name, callback) {
+    const frames = [];
+    
+    app.loader
+    .add(`assets/${name}.json`)
+    .load((file) => {
+        console.log(file.resources[`assets/${name}.json`].data)
+        let length = Object.keys(file.resources[`assets/${name}.json`].data.frames).length
+        for (let i = 0; i < length; i++) {
+            const val = i < length ? `${i}` : i;
+            // magically works since the spritesheet was loaded with the pixi loader
+            frames.push(PIXI.Texture.from(`${name}${val}.png`));
+        }
+        const anim = new PIXI.AnimatedSprite(frames);
+        callback(anim)
+    });
+}
